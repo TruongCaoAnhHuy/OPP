ac3710f5affbbbee1af122beefe729da363d49e0
TruongCaoAnhHuy <huy96qt123@gmail.com>
2022-01-20 11:24:48 +0700

    update Student

--- src/oo/Student.java
+++ src/oo/Student.java
@@ -4,8 +4,32 @@ public class Student {
     //attibute - data - state
     boolean gioitinh;
     float tuoi;
-    String ten, khoa;
+    String ten, khoa, gioitinhString;
     double diemTB;
+
+    public Student(boolean gioitinh, float tuoi, String ten, String khoa, double diemTB, String gioitinhString){
+        this.gioitinh = gioitinh;
+        this.tuoi = tuoi;
+        this.ten = ten;
+        this.khoa = khoa;
+        this.diemTB = diemTB;
+        this.gioitinhString = gioitinhString;
+    }
+
+    void show(){
+        System.out.println("Name: "+ten);
+        if(gioitinh==true){
+            gioitinhString="nam";
+        }
+        else{
+            gioitinhString = "nu";
+        }
+        System.out.println("Sex: "+gioitinhString);
+        System.out.println("Age: "+tuoi);
+        System.out.println("Department: "+khoa);
+        System.out.println("Point: "+diemTB);
+        System.out.println();
+    }
 
     //method - function - behavior
     void hoc(){
--- src/oo/StudentTestDrive.java
+++ src/oo/StudentTestDrive.java
@@ -2,14 +2,14 @@ package oo;
 
 public class StudentTestDrive {
     public static void main(String[] args) {
-        Student huyStu = new Student();
-        huyStu.hoc();
+        
+        Student huyStu = new Student(true, 20, "Anh Huy", "CNTT", 8.5, null);
+        Student aStu = new Student(false, 19, "Thị B", "KT", 9.0, null);
+        Student bStu = new Student(true, 21, "Văn A", "VH", 7.5, null);
 
-        Student aStu = new Student();
-        aStu.thi();
-            
-        Student bStu = new Student();
-        bStu.dkMon();
 
+        huyStu.show();
+        aStu.show();
+        bStu.show();
     }
 }
